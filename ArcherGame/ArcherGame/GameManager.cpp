b78aa2ddee2ce9dd1f3f65ccc33ddad14306d13e
MashedTatos <32725020+MashedTatos@users.noreply.github.com>
2020-12-03 13:57:09 -0500

    Read scores from file and store in GameManager

--- ArcherGame/ArcherGame/GameManager.cpp
+++ ArcherGame/ArcherGame/GameManager.cpp
@@ -11,4 +11,38 @@ void GameManager::Initialze()
 {
     GameEngine::Instance()->GetAudioManager()->LoadSound("Audio/drawbow.wav", AudioScope::GLOBAL, "Draw bow");
     GameEngine::Instance()->GetAudioManager()->LoadSound("Audio/bowrelease.wav", AudioScope::GLOBAL, "Bow release");
+    ReadScores("scores.txt");
+}
+
+void GameManager::ReadScores(std::string textFile) {
+    fstream file;
+    file.open(textFile, ios::in);
+    if (!file) {
+        std::cout << "Could not open file " << textFile;
+    }
+    else {
+        char ch;
+        std::vector<int> scores;
+        std::string score;
+        while (!file.eof()) {
+            file >> ch;
+            if (ch != ';' && !file.eof()) {
+                score.append(1, ch);
+            }
+            else {
+                try {
+                    int iScore = std::stoi(score);
+                    scores.push_back(iScore);
+                    std::cout << iScore << "\n";
+                    score.clear();
+                }
+                catch (std::invalid_argument e) {
+                    std::cout << "Invalid score\n";
+                }
+                
+            }
+
+        }
+        m_Scores = scores;
+    }
 }
--- ArcherGame/ArcherGame/GameManager.h
+++ ArcherGame/ArcherGame/GameManager.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "GameEngine.h"
+#include <vector>
+#include <fstream>
 class GameManager
 {
+private:
+	std::vector<int> m_Scores;
 public:
 	static GameManager* Instance();
 	void Initialze();
-
+	void ReadScores(std::string textFile);
+	std::vector<int> GetScores() { return m_Scores; }
 };
 
