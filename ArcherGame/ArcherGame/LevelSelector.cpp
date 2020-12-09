32e0c36744eb5bd0445a96cb52ad1bfe86c38cfa
jack-Protst <72477596+jack-Protst@users.noreply.github.com>
2020-12-08 23:42:59 -0500

    Update LevelSelector.cpp

--- ArcherGame/ArcherGame/LevelSelector.cpp
+++ ArcherGame/ArcherGame/LevelSelector.cpp
@@ -4,9 +4,7 @@
 #include "MainMenuState.h"
 #include "../GELib/Button.h"
 
-
 using namespace std;
-
 
 void LevelSelector::Enter()
 {
@@ -14,57 +12,42 @@ void LevelSelector::Enter()
 	m_vButtons.push_back(new Button("Img/Stages/Stage2.png", { 2,2,601,101 }, { 412,400,200,80 }));
 	m_vButtons.push_back(new Button("Img/Stages/Stage3.png", { 3,3,801,121 }, { 412,200,200,80 }));
 	m_vButtons.push_back(new Button("Img/Stages/Stage4.png", { 3,3,801,121 }, { 412,200,200,80 }));
-
-
 }
 
 void LevelSelector::Update()
 {
-
 	for (int i = 0; i < (int)m_vButtons.size(); i++)
 		m_vButtons[i]->Update();
-
 	if (m_vButtons[btn::level1]->Clicked())
 	{
-
 		GameEngine::Instance()->GetFSM()->ChangeState(new GameState());
 	}
-
 	else if (m_vButtons[btn::level2]->Clicked())
 	{
 		GameEngine::Instance()->GetFSM()->ChangeState(new GameState());
-
 	}
 	else if (m_vButtons[btn::level3]->Clicked())
 	{
 		GameEngine::Instance()->GetFSM()->ChangeState(new GameState());
-
 	}
 	else
 	{
 		GameEngine::Instance()->GetFSM()->ChangeState(new GameState());
 	}
-
 }
-
 
 void LevelSelector::Render()
 {
-
 	SDL_SetRenderDrawColor(Game::Instance()->GetRenderer(), 200, 264, 200, 200);
 	SDL_Rect rect = { 256, 128, 512, 512 };
 	SDL_RenderFillRect(Game::Instance()->GetRenderer(), &rect);
 	for (int i = 0; i < (int)m_vButtons.size(); i++)
-		m_vButtons[i]->Render();
-
+	m_vButtons[i]->Render();
 	ScreenState::Render();
-
 }
-
 
 void LevelSelector::Exit()
 {
-
 	for (int i = 0; i < (int)m_vButtons.size(); i++)
 	{
 		delete m_vButtons[i];
